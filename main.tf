816bfa55be1a361f40073826b98aadf95ee1a1e8
avasqubu <avasqubu@emeal.nttdata.com>
2024-01-08 09:39:58 -0300

    prueba 5

--- main.tf
+++ main.tf
@@ -1,19 +1,15 @@
-terraform {
-  required_providers {
-    google = {
-      source = "hashicorp/google"
-      version = "~> 3.72.0"
-    }
-  }
-}
 
 resource "google_compute_instance" "vm" {
-  name = "var.instance_name"
-  machine_type = "var.machine_type"
-  zone = "var.region"
+  name = "${var.instance_name}"
+  machine_type = "${var.machine_type}"
+  zone = "${var.region}"
   boot_disk {
     initialize_params {
-      image = "var.image"
+      image = "${var.image}"
     }
+  }
+network_interface {
+  network = "default"
+  access_config{}
   }
 }
